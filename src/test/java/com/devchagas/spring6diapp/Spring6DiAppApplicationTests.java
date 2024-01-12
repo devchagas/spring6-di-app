55d55d13703df46b685f1ba6b832d0e5c5bd8144
devgabrielchagas <gabriel.chagas2044@gmail.com>
2024-01-12 10:42:11 -0300

    add: Tests to Application Context with 2 methods, one autowiring the context and then providing the Controller bean and the other method is autowiring the controller and Spring Context brings the bean itself and we just net to print the hello message.

--- src/test/java/com/devchagas/spring6diapp/Spring6DiAppApplicationTests.java
+++ src/test/java/com/devchagas/spring6diapp/Spring6DiAppApplicationTests.java
@@ -1,10 +1,35 @@
 package com.devchagas.spring6diapp;
 
+import com.devchagas.spring6diapp.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiAppApplicationTests {
+
+	//Method 1
+	@Autowired
+	ApplicationContext applicationContext;
+
+	//Method 1
+	@Test
+	void testGetControllerFromContext() {
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
+
+	//Method 2
+	@Autowired
+	MyController myController;
+
+	//Method 2
+	@Test
+	void testAutowireOfController() {
+		System.out.println(myController.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
